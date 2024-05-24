c185eff105f72d3db0ba3338ca24a9696023c1e4
Ivan Ukhov <ivan.ukhov.work@gmail.com>
2024-05-24 14:35:36 +0600

    Created models and migrations

--- mailing_service/models.py
+++ mailing_service/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 NULLABLE = {'blank': True, 'null': True}
 
 
@@ -15,6 +14,18 @@ class Client(models.Model):
     class Meta:
         verbose_name = 'клиент'
         verbose_name_plural = 'клиенты'
+
+
+class Message(models.Model):
+    subject = models.CharField(max_length=100, verbose_name='тема сообщения')
+    body = models.TextField(verbose_name='текст сообщения')
+
+    def __str__(self):
+        return f'Сообщение №{self.pk}. Тема: {self.subject}'
+
+    class Meta:
+        verbose_name = 'сообщение'
+        verbose_name_plural = 'сообщения'
 
 
 class Newsletter(models.Model):
@@ -34,3 +45,27 @@ class Newsletter(models.Model):
     status = models.CharField(max_length=50, verbose_name='статус рассылки', choices=newsletter_status, default=created)
 
     client = models.ManyToManyField(Client, verbose_name='клиент', **NULLABLE)
+    message = models.ForeignKey(Message, on_delete=models.CASCADE, verbose_name='сообщение', **NULLABLE)
+
+    def __str__(self):
+        return f'Рассылка №{self.pk}. Время: {self.start_time} - {self.end_time}. Статус: {self.status}. Частота: {self.frequency}.'
+
+    class Meta:
+        verbose_name = 'рассылка'
+        verbose_name_plural = 'рассылки'
+
+
+class NewsletterTry(models.Model):
+    last_try_time = models.DateTimeField(verbose_name='дата и время последней попытки рассылки')
+    success = models.BooleanField(verbose_name='результат рассылки')
+    response = models.TextField(verbose_name='ответ сервера', **NULLABLE)
+
+    newsletter = models.ForeignKey(Newsletter, on_delete=models.DO_NOTHING, verbose_name='рассылка')
+    client = models.ForeignKey(Client, on_delete=models.DO_NOTHING, verbose_name='клиент')
+
+    def __str__(self):
+        return f'Попытка рассылки №{self.pk}. Дата и время: {self.last_try_time}. Результат: {self.success}.'
+
+    class Meta:
+        verbose_name = 'попытка рассылки'
+        verbose_name_plural = 'попытки рассылки'
